a5faf6d85776d1a472d314d4c138ded6bb13aca3
Charonn <charonn.edwards412@gmail.com>
2022-10-26 15:12:21 -0400

    create parseAndDisplay function

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,24 @@
-console.log('the script tag works');
+function parseAndDisplayName(name) {
+    let space = name.indexOf(' ');
+    let firstName = name.substring(0,space);
+    let lastName = name.substring(space + 1);
+    console.log(firstName, lastName, name);
+}
+
+parseAndDisplayName('Tom Cruz')
+
+
+
+
+
+
+/* console.log('the script tag works');
+
+let fullName = "Charonn Edwards";
+console.log(`the variable fullName is: ${fullName}`);
+let space = fullName.indexOf(' ');
+console.log(space);
+let fName = fullName.substring(0,space);
+console.log(fName);
+let lName = fullName.substring(space + 1);
+console.log(lName); */
